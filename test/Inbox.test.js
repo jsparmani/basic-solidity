c2140116f978fa6995da9825f5ca1e13be71da2d
Jay Parmani <jsparmani@gmail.com>
2020-02-14 22:19:59 +0530

    mocha testing basics

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,3 +3,29 @@ const ganache = require("ganache-cli");
 const Web3 = require("web3"); //importing a constructor function to use a instance of Web3
 
 const web3 = new Web3(ganache.provider()); //new instance of Web3
+
+class Car {
+    park() {
+        return "stopped";
+    }
+
+    drive() {
+        return "vroom";
+    }
+}
+
+let car;
+
+beforeEach(() => {
+    car = new Car();
+});
+
+describe("Car", () => {
+    it("can park", () => {
+        assert.equal(car.park(), "stopped");
+    });
+
+    it("can drive", () => {
+        assert.equal(car.drive(), "vroom");
+    });
+});

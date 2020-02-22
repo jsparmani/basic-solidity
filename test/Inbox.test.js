b68571121048a6c1c28e2051dfecfe82276bab76
Jay Parmani <jsparmani@gmail.com>
2020-02-22 21:23:11 +0530

    testing part1

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,7 +4,33 @@ const Web3 = require("web3"); //importing a constructor function to use a instan
 
 const web3 = new Web3(ganache.provider()); //new instance of Web3
 
-class Car {
+const {interface, bytecode} = require("../compile");
+
+let accounts, inbox;
+
+beforeEach(async () => {
+    //Get a list of all accounts
+    accounts = await web3.eth.getAccounts();
+
+    //Use one of the accounts to deploy the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface)).deploy({
+        data: bytecode,
+        arguments: ["Hi there!"]
+    })
+    .send({from: accounts[0], gas: "1000000"});
+});
+
+describe("Inbox", () => {
+    it("deploys a contract", () => {
+        assert.ok(inbox.options.address);
+    });
+
+    it('has a default message', () => {
+        const message = await inbox.methods.message().call();
+    })
+});
+
+/* class Car {
     park() {
         return "stopped";
     }
@@ -28,4 +54,4 @@ describe("Car", () => {
     it("can drive", () => {
         assert.equal(car.drive(), "vroom");
     });
-});
+}); */
